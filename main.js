3d9efc5e21ea1652ab97714e2d6627638b7fddc1
Ezra Marks <ezrablankmarks@gmail.com>
2021-03-28 15:36:33 -0400

    Adjust graph layout

--- main.js
+++ main.js
@@ -4,26 +4,26 @@ colors.red = "#E50914";
 colors.darkRed = "#B20710";
 colors.darkGray = "#292929";
 
-const MAX_WIDTH = Math.max(1080, window.innerWidth);
-const MAX_HEIGHT = 720;
+const WIDTH = Math.min(960, window.innerWidth * 0.8);
+const HEIGHT = 540;
 const margin = {top: 40, right: 100, bottom: 40, left: 175};
 
 // GRAPH 1
-const graph1Width = (MAX_WIDTH / 2) - 10;
-const graph1Height = 350;
+const graph1Width = WIDTH;
+const graph1Height = HEIGHT;
 const graph1NumExamples = 15;
 
 // GRAPH 2
 const graph2 = {};
-graph2.width = (MAX_WIDTH / 2) - 10;
-graph2.height = 275;
+graph2.width = WIDTH;
+graph2.height = HEIGHT;
 graph2.margin = margin;
 graph2.maxNumTicks = 5; // NOTE: Must be multiple of 2 or 5
 
 // GRAPH 3
 const graph3 = {};
-graph3.width = MAX_WIDTH / 2;
-graph3.height = 575;
+graph3.width = WIDTH
+graph3.height = HEIGHT;
 graph3.margin = margin;
 
 
